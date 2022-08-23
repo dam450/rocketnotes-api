a3814b84f49aad83ed82db1a537860280f25ab93
Dam450 <68165752+dam450@users.noreply.github.com>
2022-08-23 15:19:28 -0300

    install knex.js and config connection
    	new file:   knexfile.js
    	new file:   src/database/knex/index.js
    	modified:   package-lock.json
    	modified:   package.json

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+const path = require("path")
+
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    useNullAsDefault: true
+  }
+
+};
