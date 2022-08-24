32b7de6a4c0a54442146cdbcf1e6888c460b98a7
Dam450 <68165752+dam450@users.noreply.github.com>
2022-08-24 18:14:34 -0300

    add controllers to create and show notes
    	new file:   src/controllers/NotesController.js
    	modified:   src/routes/index.js
    	new file:   src/routes/notes.routes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const { Router } = require('express') // requisita a lib express
 
 const usersRouter = require('./users.routes') // requisita o arquivo de rotas do usuário
+const notesRouter = require('./notes.routes')
 
 const routes = Router() 
 
 //routes recebe as rotas de usersRouter
 routes.use('/users', usersRouter) // rota com "/users" use as rotas de usersRouter
+
+routes.use('/notes', notesRouter)
 
 module.exports = routes
