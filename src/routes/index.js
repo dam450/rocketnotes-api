1565bfd41c04c81c93b5d40a40d92a085a8447bb
Dam450 <68165752+dam450@users.noreply.github.com>
2022-08-26 15:55:15 -0300

    add tags route and controller
    lists tags of a user with index method
    	new file:   src/controllers/TagsController.js
    	modified:   src/routes/index.js
    	new file:   src/routes/tags.routes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ const { Router } = require('express') // requisita a lib express
 
 const usersRouter = require('./users.routes') // requisita o arquivo de rotas do usuário
 const notesRouter = require('./notes.routes')
+const tagsRouter  = require('./tags.routes')
 
 const routes = Router() 
 
-//routes recebe as rotas de usersRouter
+//routes recebe as rotas 
 routes.use('/users', usersRouter) // rota com "/users" use as rotas de usersRouter
-
-routes.use('/notes', notesRouter)
+routes.use('/notes', notesRouter) // rota com "/notes" use as rotas de notesRouter
+routes.use('/tags', tagsRouter)   // rota com "/tags" use as rotas de tagsRouter
 
 module.exports = routes
